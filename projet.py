3d0ad08293e6bdadc36af3c3bf17868832ef0206
Hanako29 <57670575+Hanako29@users.noreply.github.com>
2019-11-14 16:05:00 +0100

    extraction et remplissage fini

--- projet.py
+++ projet.py
@@ -7,8 +7,8 @@ import re, sys
 ##Lecture du fichier et extraction
 
 def main(inputSam) :
-    dicoExt = {}
-    extraction(inputSam)
+    print(extraction(inputSam))
+   
 
 def extraction(inputSam) :
     with open("mapping.sam", "r") as fSam :
@@ -16,65 +16,52 @@ def extraction(inputSam) :
 
         next(fSam) #Saute la 1ere ligne
         next(fSam) #Saute la 2eme ligne
+
+        dicoExt = {} #dico d'extraction
+        #flag = []
+        #nomReads = []
+        #cigar = []
+        #tagMDZ = []
         
         for reads in fSam :
-
             col = reads.split("\t")
-
+            #flag.append(col[1])
+            #nomReads.append(col[0])
+            #cigar.append(col[5])
             flag = col[1]
-            #print(flag)
-
             nomReads = col[0]
-            #print(nomReads)
-
             cigar = col[5]
-            #print(cigar)
+            
+            resTagMDZ = re.search("(MD\:Z\:.*?\t)", reads)
+            if resTagMDZ :
+                resTagMDZ = resTagMDZ.group(0)[:-1] #[:-1] enlève \t
+                #tagMDZ.append(resTagMDZ)
+                tagMDZ = resTagMDZ
+            remplissage(dicoExt,flag,nomReads,cigar, tagMDZ)
 
-            tagMDZ = col[8]
-            print(tagMDZ)
+        print("Fin extraction")
+        
+        return dicoExt
+        
 
+        #print(flag)
+        #print(nomReads)
+        #print(cigar)
+        #print(tagMDZ)
+        #print(len(flag))
+        #print(len(nomReads))
+        #print(len(cigar))
+        #print(len(tagMDZ))
+        #Même longueur de la liste partout sauf tagMDZ (mais c'est une info optionnelle)
 
-            #refFlag = re.search("(^.*?\t\d{1,4}|^.*?\t\*)", reads)
-            #if refFlag : #car match pas les 2 premières lignes
-            #    resRefFlag = refFlag.group(0)
-                #print(resRefFlag)
-            #    col = resRefFlag.split("\t")
-            #    flag = col[1]
-                #print(flag)
-                #nomReads
-            #    nomReads = col[0]
-                #print(nomReads)
-                #cigar
-            #    resCig = re.search("(([0-9]+[MIDNSHPX=]+)+\t)", reads)
-            #    if resCig:
-            #        cigar = resCig.group(0)
-                    #print(cigar)
-                #tag
-            #    resTag=re.search("(MD\:Z\:([0-9]+[ATGC])+|MD\:Z\:([0-9])+)")
-            #    if resTag :
-            #        tag = resTag.group(0)
-            #        print(tag)
-                
-                
-                #if flag in dicoExt :
-                #    dicoExt[flag] = append([nomReads])
-                #else :
-                #    dicoExt[flag] = 
-                    
-        #print("nombre d'occurence flags", len(nomReads))
-        #print("nombre d'occurence nomReads", )    
-        #print("nombre d'occurence cigar",)
-            #Remplissage du dico avec
-            #cigar = re.search("(([0-9]+[MIDNSHPX=])+)")]
-            #if flag in dicoExt :
-            #    dicoExt[flag] = desc.append([])
-            #else :
-            #dicoExt[flag] = [[re.search("(([0-9]+[MIDNSHPX=])+)")]]
-        #print(dicoExt)
+        
+    
+def remplissage(dicoExt, flag, nomReads, cigar, tagMDZ) :
 
-
-
-
+    if flag in dicoExt :
+        dicoExt[flag][nomReads] = [cigar, tagMDZ]
+    else :
+        dicoExt[flag] = {nomReads : [cigar, tagMDZ]}
 
         
 if len(sys.argv) == 2 :
